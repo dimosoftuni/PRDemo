cd0cd454df444728a6d90e8818adee15e06ec36c
Dimo Mitev <dimosoftuni@gmail.com>
2026-01-15 22:05:21 +0200

    fixes based on the review

--- calculator.java
+++ calculator.java
@@ -7,6 +7,8 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
+
+    // Multiplication method
     public int multiply(int a, int b) {
         return a * b;
     }
